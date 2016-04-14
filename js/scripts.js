7971c4a2a2e478f9b65dabf7d02c153a0d2bb062
Ned Faulhaber <nedfaulhaber@gmail.com>
2016-04-14 13:25:58 -0700

    add borders for images of shop recommendations, add output for javascript, edit id names in output section of html

--- js/scripts.js
+++ js/scripts.js
@@ -64,18 +64,26 @@ $(document).ready(function(){
   // var location;
   // var output;
 
-  var starb = new Coffee("Starbucks", "1", "1", "2", "1", "2", "1", "2", "1", "6", 0);
-  var dutch = new Coffee("Dutch Bros.", "2", "1", "3", "1", "1", "2", "1", "1", "6", 0);
-  var woodl = new Coffee("Woodlawn Coffee & Pastry", "3", "1", "2", "2", "3", "1", "3", "2", "2", 0);
-  var baris = new Coffee("Barista", "4", "2", "1", "2", "4", "1", "4", "2", "2", 0);
-  var drago = new Coffee("DragonFly Coffee House", "3", "1", "2", "2", "3", "1", "3", "2", "1", 0);
-  var sterl = new Coffee("Sterling Coffee Roasters", "4", "2", "1", "2", "4", "1", "4", "2", "1", 0);
-  var annab = new Coffee("Anna Bannanas", "3", "1", "2", "2", "3", "1", "3", "2", "4", 0);
-  var cathe = new Coffee("Cathedral Coffee", "4", "2", "1", "2", "4", "1", "4", "2", "4", 0);
-  var fresh = new Coffee("The Fresh Pot", "3", "1", "2", "2", "3", "1", "3", "2", "4", 0);
-  var eithe = new Coffee("Either/Or", "4", "2", "1", "2", "4", "1", "4", "2", "4", 0);
-  var fehre = new Coffee("Fehrenbacher Hof", "3", "1", "2", "2", "3", "1", "3", "2", "3", 0);
-  var heart = new Coffee("Heart Roasters", "4", "2", "1", "2", "4", "1", "4", "2", "3", 0);
+  var starb1 = new Coffee("Starbucks", "1", "1", "2", "1", "2", "1", "2", "1", "NE", 0);
+  var starb2 = new Coffee("Starbucks", "1", "1", "2", "1", "2", "1", "2", "1", "SE", 0);
+  var starb3 = new Coffee("Starbucks", "1", "1", "2", "1", "2", "1", "2", "1", "NW", 0);
+  var starb4 = new Coffee("Starbucks", "1", "1", "2", "1", "2", "1", "2", "1", "SW", 0);
+  var starb5 = new Coffee("Starbucks", "1", "1", "2", "1", "2", "1", "2", "1", "N", 0);
+  var dutch1 = new Coffee("DutchBros", "2", "1", "3", "1", "1", "2", "1", "1", "NE", 0);
+  var dutch2 = new Coffee("DutchBros", "2", "1", "3", "1", "1", "2", "1", "1", "SE", 0);
+  var dutch3 = new Coffee("DutchBros", "2", "1", "3", "1", "1", "2", "1", "1", "NW", 0);
+  var dutch4 = new Coffee("DutchBros", "2", "1", "3", "1", "1", "2", "1", "1", "SW", 0);
+  var dutch5 = new Coffee("DutchBros", "2", "1", "3", "1", "1", "2", "1", "1", "N", 0);
+  var woodl = new Coffee("Woodlawn", "3", "1", "2", "2", "3", "1", "3", "2", "NE", 0);
+  var baris = new Coffee("Barista", "4", "2", "1", "2", "4", "1", "4", "2", "NE", 0);
+  var drago = new Coffee("DragonFly", "3", "1", "2", "2", "3", "1", "3", "2", "NW", 0);
+  var sterl = new Coffee("Sterling", "4", "2", "1", "2", "4", "1", "4", "2", "NW", 0);
+  var annab = new Coffee("AnnaBannanas", "3", "1", "2", "2", "3", "1", "3", "2", "N", 0);
+  var cathe = new Coffee("Cathedral", "4", "2", "1", "2", "4", "1", "4", "2", "N", 0);
+  var fresh = new Coffee("FreshPot", "3", "1", "2", "2", "3", "1", "3", "2", "SE", 0);
+  var eithe = new Coffee("EitherOr", "4", "2", "1", "2", "4", "1", "4", "2", "SE", 0);
+  var fehre = new Coffee("FehrenbacherHof", "3", "1", "2", "2", "3", "1", "3", "2", "SW", 0);
+  var heart = new Coffee("HeartRoasters", "4", "2", "1", "2", "4", "1", "4", "2", "SW", 0);
 
   $("form#collect").submit(function(event){
     event.preventDefault();
@@ -96,7 +104,7 @@ $(document).ready(function(){
     // coffee objects
     var userCoffee = new Coffee(name, importAmbi, importConv, importCuli, importCons, importWait, importWifi, importPric, importFlav, importLoca, count);
     // var newFulls = new Full (starb,dutch,woodl,baris,drago,sterl,annab,cathe,fresh,eithe,fehre,heart);
-    var stores = [starb, dutch, woodl, baris, drago, sterl, annab, cathe, fresh, eithe, fehre, heart];
+    var stores = [starb1, starb2, starb3, starb4, starb5, dutch1, dutch2, dutch3, dutch4, dutch5, woodl, baris, drago, sterl, annab, cathe, fresh, eithe, fehre, heart];
 
     // these 9 loops check for matching between user properties and coffeshop properties and increments the "count" property for the coffeeshop that macthes
 
@@ -148,8 +156,6 @@ $(document).ready(function(){
 
 // [starb, dutch, woodl, baris, drago, sterl, annab, cathe, fresh, eithe, fehre, heart];
 
-console.log(starb.count);
-console.log(dutch.count);
 console.log(woodl.count);
 console.log(baris.count);
 console.log(drago.count);
@@ -161,10 +167,11 @@ console.log(eithe.count);
 console.log(fehre.count);
 console.log(heart.count);
 
-  var outputStores = [];
-  var highestCount = 0;
+    var outputStores = [];
+    var highestCount = 0;
 console.log(highestCount);
-  stores.forEach(function(store) {
+
+    stores.forEach(function(store) {
     if (store.loca === userCoffee.loca){
       if (store.count >= highestCount) {
         highestCount = store.count;
@@ -177,16 +184,35 @@ console.log(highestCount);
     if (store.count === highestCount && store.loca === userCoffee.loca) {
       outputStores.push(store.name);
     }
-    if (store.count === highestCount && store.loca === "6") {
-      outputStores.push(store.name);
-    }
+    // if (store.count === highestCount && store.loca === "6") {
+    //   outputStores.push(store.name);
+    // }
   });
 console.log(outputStores);
 
-$("#output").fadeIn();
-$("form#collect").fadeOut();
+    $("#output").show();
+    $("#collect").hide();
 
 
+    // if (outputStores[0] === "Starbucks"){
+    //   $("#starbucksNE").show()
+    // }
+
+    stores.forEach(function(store) {
+      for (var index = 0; index <= outputStores.length; index +=1) {
+        if (store.name === outputStores[index] && store.loca === importLoca) {
+          $("#" + store.name + importLoca).show();
+        }
+        if (store.name === outputStores[index] && store.loca === importLoca) {
+          $("#" + store.name).show();
+        }
+      }
+    });
+
+    reset(); // clears user inputs
+
+  });
+});
 
 // these 9 loops check for matching between user properties and coffeshop properties and increments the "count" property for the coffeeshop that macthes
     // newFulls.compile.forEach(function(newFull){
@@ -261,9 +287,3 @@ $("form#collect").fadeOut();
     //   $("#output").show();
     //   $("#dutch-bros").show();
     // }
-
-
-    reset(); // clears user inputs
-
-  });
-});
